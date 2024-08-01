f8484469c508f5b6024b977c344367d903d634b2
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-08-01 20:11:17 +0000

    updated get_todo_list view function and added template variables to todo_list.html

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render, HttpResponse
+from .models import item
 
 # Create your views here.
+
+"""Gets items data from database
+
+Creates the item variable by assigning it the item class imported from
+models.py and the Django QuerySet method .objects.all(), which retrieves all
+instances of the item model from the database. The query set is then passed to
+the context dictionary as the value in the key value pair 'items':item and
+finally the dictionary is made available to the html template by adding it to
+the return render function.
+"""
+
 def get_todo_list(request):
-    return render(request, "todo/todo_list.html")
+    # Creates item variable with .objects.all()
+    items = item.objects.all()
+    # Creates dictionary using items variable
+    context = {
+        'items':items
+    }
+    # Makes context dictionary available to html template
+    return render(request, "todo/todo_list.html", context)
