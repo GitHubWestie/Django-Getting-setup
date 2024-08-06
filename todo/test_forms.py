9d7c2a6e36616455a0ddcc6c926093de27b6777b
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-08-06 14:58:50 +0000

    wrote tests for forms.py

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -1,1 +1,21 @@
 from django.test import TestCase
+from .forms import item_form
+
+
+class TestItemForm(TestCase):
+
+    def test_item_name_is_required(self):
+        form = item_form({'name' : ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+
+    def test_done_field_is_not_required(self):
+        form = item_form({'name' : 'Test_done_field'})
+        self.assertTrue(form.is_valid())
+
+
+    def test_fields_are_explicit_in_form_meta_class(self):
+        form = item_form()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
