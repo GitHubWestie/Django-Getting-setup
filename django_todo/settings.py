d3e1ae1f09b1dfdcbf272e269c4c75e55df54b31
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-08-08 14:31:12 +0000

    fixed host variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('HEROKU_HOST')
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOST')]
 
 
 # Application definition
